0eaeb981b1179e8320863fbfaf50da4432b30180
Ai231001 <wasamjaffri@gmail.com>
2024-10-22 22:09:47 -0700

    Add files via upload

---
+++ MatrixAddition.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author 123
+ */
+
+public class MatrixAddition {
+    public static void main(String[] args) {
+        int[][] matrix1 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] matrix2 = {
+            {9, 8, 7},
+            {6, 5, 4},
+            {3, 2, 1}
+        };
+
+        int[][] sumMatrix = addMatrices(matrix1, matrix2);
+
+        System.out.println("Sum of the two matrices:");
+        printMatrix(sumMatrix);
+    }
+
+    // Method to add two matrices
+    public static int[][] addMatrices(int[][] m1, int[][] m2) {
+        int rows = m1.length;
+        int cols = m1[0].length;
+        int[][] result = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = m1[i][j] + m2[i][j];
+            }
+        }
+        return result;
+    }
+
+    // Method to print a matrix
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
