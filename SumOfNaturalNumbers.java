e5a7136f4fc182765096345ce219e33044b6cc41
wasam0110 <wasamjaffri@gmail.com>
2024-10-22 21:31:55 -0700

    Add files via upload
    Sum of first 10 natural numbers

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author 123
+ */
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        int n = 10; // First 10 natural numbers
+        int sum = n * (n + 1) / 2; // Using the formula
+
+        System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
+    }
+}
