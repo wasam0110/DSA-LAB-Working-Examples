fd5d5cd110135d69a85991af10598e0f623a66eb
Ai231001 <wasamjaffri@gmail.com>
2024-11-20 12:50:48 -0800

    Add files via upload

---
+++ ArrayMinimum.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author wasam
+ */
+public class ArrayMinimum {
+
+    public static int findMin(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 2, 8, 1, 3};
+        System.out.println("Minimum value: " + findMin(array));
+    }
+}
+
