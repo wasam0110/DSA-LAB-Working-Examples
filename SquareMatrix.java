2170d0f28d8984ba7211382f8fc764cda52c0e09
Ai231001 <wasamjaffri@gmail.com>
2024-11-20 12:54:24 -0800

    Add files via upload

---
+++ SquareMatrix.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author wasam
+ */
+public class SquareMatrix {
+
+    public static int[][] transpose(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transposed = new int[cols][rows];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
+    }
+
+    public static int leftDiagonalSum(int[][] matrix) {
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            sum += matrix[i][i];
+        }
+        return sum;
+    }
+
+    public static int rightDiagonalSum(int[][] matrix) {
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            sum += matrix[i][matrix.length - 1 - i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] transposedMatrix = transpose(matrix);
+        System.out.println("Transposed Matrix:");
+        for (int[] row : transposedMatrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Left Diagonal Sum: " + leftDiagonalSum(matrix));
+        System.out.println("Right Diagonal Sum: " + rightDiagonalSum(matrix));
+    }
+}
+
