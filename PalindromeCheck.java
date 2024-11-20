f811a3d2ff1862ea189db668219cd8f22cef8398
Ai231001 <wasamjaffri@gmail.com>
2024-11-20 12:53:11 -0800

    Add files via upload

---
+++ PalindromeCheck.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author wasam
+ */
+public class PalindromeCheck {
+
+    public static boolean isPalindrome(String str) {
+        int start = 0, end = str.length() - 1;
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "madam";
+        System.out.println("Is palindrome: " + isPalindrome(str));
+    }
+}
+
